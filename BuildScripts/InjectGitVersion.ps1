df8c908ed8c82539149b9e7397bee8aff95740d3
Mariusz Brzeski <marianob85@gmail.com>
2019-05-19 11:51:50 +0200

    Modified   BuildScripts/InjectGitVersion.ps1

--- BuildScripts/InjectGitVersion.ps1
+++ BuildScripts/InjectGitVersion.ps1
@@ -1,27 +1,32 @@
-# InjectGitVersion.ps1
-#
-# Set the version in the projects AssemblyInfo.cs file
-#
 
-$productVersion = "1.4.0.";
+ param (
+    [Parameter(Mandatory=$true)][string]$Version
+ )
+
+$productVersion = "1.4.";
+$revision = "0";
+
+$fileNames = Get-ChildItem -Path (Split-Path -Parent $PSScriptRoot) -Recurse -Include *.template
 
 $gitVersion = git describe --all --long --always --first-parent;
 $gitVersion -match '.*/([\w\d]+)-(\d+)-[g](\w+)$';
 $gitTag = $Matches[1];
-$gitCount = $Matches[2];
 $gitSHA1 = $Matches[3];
 
-# Define file variables
-$assemblyFile = $args[0] + "\Properties\AssemblyInfo.cs";
-$templateFile =  $args[0] + "\Properties\AssemblyInfo_template.cs";
+foreach ($fileName in $fileNames) {
+    # Define file variables
+    $file = Get-Item $fileName;
+    $assemblyFile = ( Join-Path $file.DirectoryName  $file.BaseName );
 
-# Read template file, overwrite place holders with git version info
-$newAssemblyContent = Get-Content $templateFile |
-    %{$_ -replace '\$FILEVERSION\$', ($productVersion + $gitCount) } |
-    %{$_ -replace '\$INFOVERSION\$', ($productVersion + $gitCount + "-" + $gitTag + "-" + $gitSHA1) };
+    # Read template file, overwrite place holders with git version info
+    $newAssemblyContent = Get-Content $fileName |
+        %{$_ -replace '\$FILEVERSION\$', ($productVersion + $Version + "." + $revision) } |
+        %{$_ -replace '\$INFOVERSION\$', ($productVersion + $Version + "." + $revision + "-" + $gitTag + "-" + $gitSHA1) };
 	
-# Write AssemblyInfo.cs file only if there are changes
-If (-not (Test-Path $assemblyFile) -or ((Compare-Object (Get-Content $assemblyFile) $newAssemblyContent))) {
-    echo "Injecting Git Version Info to AssemblyInfo.cs"
-    $newAssemblyContent > $assemblyFile;       
+    # Write AssemblyInfo.cs file only if there are changes
+    If (-not (Test-Path $assemblyFile) -or ((Compare-Object (Get-Content $assemblyFile) $newAssemblyContent))) {
+        Write-Host "Injecting Git Version Info to $assemblyFile" ;
+        $newAssemblyContent > $assemblyFile;       
+    }  
 }
+
--- Jenkinsfile
+++ Jenkinsfile
@@ -12,8 +12,8 @@ pipeline
 	{
 		stage('Build'){
 			steps {
+				powershell '.\BuildScripts\InjectGitVersion.ps1 -Version $env:BUILD_NUMBER'
 				bat '''
-					//call "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\Common7\\Tools\\VsMSBuildCmd.bat"
 					call "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\VC\\Auxiliary\\Build\\vcvars64.bat"
 					nuget restore CodeBeautifier.sln
 					msbuild CodeBeautifier.sln /t:Rebuild /p:Configuration=Release;Platform="Any CPU" /flp:logfile=warnings.log;warningsonly'''
